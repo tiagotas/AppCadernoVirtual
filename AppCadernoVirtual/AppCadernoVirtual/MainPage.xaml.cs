81d55bee8241bb93758743759b03dbcca28442b9
Usuario <Usuario@CPD02-M00>
2022-04-12 11:59:23 -0300

    Adicionar arquivos de projeto.

---
+++ AppCadernoVirtual/AppCadernoVirtual/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using AppCadernoVirtual.Primeiro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCadernoVirtual
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PortuguesPrimeiro());
+        }
+    }
+}
